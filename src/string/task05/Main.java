c1692d7919b59d6057939a5eb97a8568eeb666ab
pvysotski <pavelve83@mail.ru>
2020-05-17 12:51:05 +0300

    update

---
+++ src/string/task05/Main.java
@@ -0,0 +1,18 @@
+package string.task05;
+
+public class Main {
+    /*
+    Удалить в строке все лишние пробелы, то есть серии подряд идущих пробелов заменить на одиночные пробелы.
+    Крайние пробелы в строке удалить.
+    */
+    public static void main(String[] args) {
+        String str = "  Удалить в   строке     все          лишние    пробелы. ";
+        System.out.println(str);
+        String newStr = str.trim();
+        while (newStr.contains("  ")) {
+            String temp = newStr.replaceAll("  ", " ");
+            newStr = temp;
+        }
+        System.out.println(newStr);
+    }
+}
