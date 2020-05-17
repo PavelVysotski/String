d74a6a20975bec8999b1b9c555b27546af29adb7
pvysotski <pavelve83@mail.ru>
2020-05-17 19:01:55 +0300

    update

--- src/string/chararray/task05/Main.java
+++ src/string/chararray/task05/Main.java
@@ -6,17 +6,22 @@ public class Main {
     Крайние пробелы в строке удалить.
     */
     public static void main(String[] args) {
-        String str = "  Удалить в   строке     все          лишние    пробелы. ";
+        String str = "  Удалить в   строке     все          лишние    пробелы    . ";
         System.out.println(str);
 
         deleteSpace(str);
     }
 
-    public static void deleteSpace (String str){
+    public static void deleteSpace(String str) {
         String newStr = str.trim();
         while (newStr.contains("  ")) {
             String temp = newStr.replaceAll("  ", " ");
             newStr = temp;
+        }
+        char ch = newStr.charAt(newStr.length()-2);
+        if (ch == ' '){
+            String newStr2 = newStr.substring(0,newStr.length()-2)+newStr.substring(newStr.length()-1);
+            newStr=newStr2;
         }
         System.out.println(newStr);
     }
