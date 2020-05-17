98dc228b9391575057c3f8007c072796c0f4387e
pvysotski <pavelve83@mail.ru>
2020-05-17 14:22:20 +0300

    update

---
+++ src/string/task07/Main.java
@@ -0,0 +1,13 @@
+package string.task07;
+
+public class Main {
+    /*
+    В строке вставить после каждого символа 'a' символ 'b'.
+    */
+    public static void main(String[] args) {
+
+        String str = "aaaa aaa aa a";
+        System.out.println(str.replaceAll("a","ab"));
+    }
+
+}
