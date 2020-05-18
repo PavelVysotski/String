ad1480260e98f6a674a59d9f070eddfddab01d9f
pvysotski <pavelve83@mail.ru>
2020-05-18 15:01:02 +0300

    update

--- src/string/objs/task07/Main.java
+++ src/string/objs/task07/Main.java
@@ -9,21 +9,21 @@ public class Main {
     def", то должно быть выведено "abcdef".
     */
     public static void main(String[] args) {
-        String str = " aab bbc c cdef fff  ";
+        String str = " ffaccab bbc c cdef fff  ";
 
         deleteRepeatAndSpace(str);
     }
 
-    public static String deleteRepeatAndSpace (String str){
-        StringBuilder sb = new StringBuilder();
+    public static String deleteRepeatAndSpace (String str){ //Метод работает только если надо сделать в алфавитном порядке.
+        StringBuilder sb = new StringBuilder();             
         if(!str.isEmpty()) {
-            char[]chars = str.toCharArray();
-            Arrays.sort(chars);
+            char[] ch = str.toCharArray();
+            Arrays.sort(ch);
 
-            sb.append(chars[0]);
-            for (int i = 1; i < chars.length; i++) {
-                if (chars[i]!= chars[i - 1]) {
-                    sb.append(chars[i]);
+            sb.append(ch[0]);
+            for (int i = 1; i < ch.length; i++) {
+                if (ch[i]!= ch[i - 1]) {
+                    sb.append(ch[i]);
                 }
             }
         }
---
+++ src/string/objs/task08/Main.java
@@ -0,0 +1,23 @@
+package string.objs.task08;
+
+public class Main {
+    /*
+    Вводится строка слов, разделенных пробелами. Найти самое длинное слово и вывести его на экран. Случай, когда самых
+    длинных слов может быть несколько, не обрабатывать.
+    */
+    public static void main(String[] args) {
+        String str = "Найти самое длинное слово в предложении и вывести его на экран.";
+        longWord(str);
+
+    }
+    public static void longWord (String s){
+        String[] strings = s.split(" ");
+        String max = "";
+        for ( String word : strings ) {
+            if (word.length() > max.length()) {
+                max = word;
+            }
+        }
+        System.out.println(max);
+    }
+}
